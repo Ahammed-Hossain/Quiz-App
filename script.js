44868909f0185551b2fb97c99ca8d7e6f53cb32f
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-09-20 09:24:42 +0600

    Working on js

---
+++ script.js
@@ -0,0 +1,12 @@
+let Next = document.querySelector('.next');
+let firstOp = document.getElementById('firstOpText');
+let secondOp = document.getElementById('secondOpText');
+let thirdOp = document.getElementById('thirdOpText');
+let forthOp = document.getElementById('forthOpText');
+
+Next.addEventListener('click', function () {
+    console.log("next clicked")
+    firstOp.innerText = "Html Tags";
+    
+});
+
